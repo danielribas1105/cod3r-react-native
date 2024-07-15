b9166780b835937866ba858a9d8a0fc7871d5638
Daniel <danielribas1105@gmail.com>
2024-07-15 00:05:32 -0300

    Create react native project

---
+++ app/primeiro.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { View, Text} from "react-native";
+import styles from "../constants/styles";
+
+export default function Primeiro() {
+    return (
+        <View style={styles.centralizado}>
+            <Text>Primeiro Componente</Text>
+        </View>
+    )
+}
